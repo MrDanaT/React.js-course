aff33d4f8b02bf864b232cab355ff98d37e49c34
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2019-10-02 01:29:40 +0200

    End of video 48
    Working with inline styles

--- course/src/App.js
+++ course/src/App.js
@@ -36,12 +36,22 @@ class App extends Component {
   }
 
   render() {
+    const styling = {
+      backgroundColor: 'red',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>Paragraph!</p>
         {/* Is een unanonymous call, kan niet efficiënt zijn! */}
-        <button onClick={() => this.buttonClickHandler('Maximilian')}>Switch Name</button>
+        <button
+          onClick={() => this.buttonClickHandler('Maximilian')}
+          style={styling}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
