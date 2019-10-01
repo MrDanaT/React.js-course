b008d8dc2d8201017154a1890cb29fb614a04722
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2019-10-02 00:16:59 +0200

    End of video 33

--- course/src/App.js
+++ course/src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>Paragraph!</p>
         <Person />
+        <Person />
+        <Person />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
