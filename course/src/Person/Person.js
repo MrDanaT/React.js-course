2ba3bbfb9a2d3d11da45b9e5cb1c2d231bc063fd
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2019-10-02 01:26:32 +0200

    End of video 47

--- course/src/Person/Person.js
+++ course/src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css';
 
 // Stateless component, ook wel dumb component genoemd aangezien ze iets presenteren, iets outputten & geen interne logica hebben.
 export const person = (props) => {
     return (
-        <div onClick={props.click}>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className='Person'>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input
                 type="text" onChange={props.changed}
