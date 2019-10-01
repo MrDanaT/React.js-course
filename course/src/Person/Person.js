165bb71a297771299590651c3cb3d81d42edbc33
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2019-10-02 00:19:02 +0200

    End of video 34

--- course/src/Person/Person.js
+++ course/src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const person = () => {
-    return <p>I'm a person!</p>;
+    return <p>I'm a person and I am {Math.floor(Math.random() * 30)} years old!</p>;
 }
