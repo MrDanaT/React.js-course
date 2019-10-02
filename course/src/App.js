10af62e4a314e6d44634bc2ce5e2b0874b473908
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2019-10-02 23:36:54 +0200

    End of video 66

--- course/src/App.js
+++ course/src/App.js
@@ -69,12 +69,20 @@ class App extends Component {
         </div>);
 
       styling.backgroundColor = 'red'
-    }
+    };
+
+    const classes = [];
+
+    if (this.state.persons.length <= 2)
+      classes.push('red');  // classes = ['red']
+
+    if (this.state.persons.length <= 1)
+      classes.push('bold'); // classes = ['red', 'bold']
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>Paragraph!</p>
+        <p className={classes.join(' ')}>Paragraph!</p>
 
         <button
           onClick={this.togglePersonHandler}
